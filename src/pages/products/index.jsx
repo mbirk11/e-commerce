a5b5296984ae85298bed6c177acb56fb432e3674
Mary <mbirkadze11@gmail.com>
2023-11-17 14:19:45 +0400

    1

--- src/pages/products/index.jsx
+++ src/pages/products/index.jsx
@@ -13,10 +13,8 @@ const Products = () => {
   }, []);
   async function fetchProducts() {
     try {
-      const response = await axios.get(
-        "https://api.escuelajs.co/api/v1/products"
-      );
-      const productData = response.data;
+      const response = await axios.get("https://dummyjson.com/products");
+      const productData = response.data.products;
       setProducts(productData);
     } catch (e) {
       console.error(e);
@@ -49,7 +47,7 @@ const Products = () => {
         ))}
       </div> */}
       <div
-        tabindex="0"
+        tabIndex="0"
         className="focus:outline-none flex flex-wrap p-4 bg-gray-100"
       >
         {products.map((product) => (
@@ -58,14 +56,14 @@ const Products = () => {
             className="flex flex-wrap items-center lg:justify-between justify-center "
           >
             <div
-              tabindex="0"
+              tabIndex="0"
               className="focus:outline-none mx-2 w-72 xl:mb-0 mb-8"
             >
               <div className="shadow duration-150 hover:scale-105 hover:shadow max-w-xs cursor-pointer ">
                 <img
                   alt={product.title}
-                  src={product.images}
-                  tabindex="0"
+                  src={product.images[0]}
+                  tabIndex="0"
                   className="focus:outline-none w-full h-44 rounded-lg"
                 />
               </div>
@@ -74,16 +72,16 @@ const Products = () => {
                   <div>
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
-                      tabindex="0"
+                      tabIndex="0"
                       className="focus:outline-none"
                       width="20"
                       height="20"
                       viewBox="0 0 24 24"
-                      stroke-width="1.5"
+                      strokeWidth="1.5"
                       stroke="#2c3e50"
                       fill="none"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                     >
                       <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                       <path d="M9 4h6a2 2 0 0 1 2 2v14l-5-3l-5 3v-14a2 2 0 0 1 2 -2"></path>
@@ -91,7 +89,7 @@ const Products = () => {
                   </div>
                   <div className="bg-yellow-200 py-1.5 px-6 rounded-full">
                     <p
-                      tabindex="0"
+                      tabIndex="0"
                       className="focus:outline-none text-xs text-yellow-700"
                     >
                       Featured
@@ -101,20 +99,20 @@ const Products = () => {
                 <div className="p-4">
                   <div className="flex items-center">
                     <h2
-                      tabindex="0"
+                      tabIndex="0"
                       className="focus:outline-none text-lg font-semibold"
                     >
                       {product.title}
                     </h2>
                     <p
-                      tabindex="0"
+                      tabIndex="0"
                       className="focus:outline-none text-xs text-gray-600 pl-5"
                     >
                       4 days ago
                     </p>
                   </div>
                   <p
-                    tabindex="0"
+                    tabIndex="0"
                     className="focus:outline-none text-xs text-gray-600 mt-2"
                   >
                     The Apple iPhone XS is available in 3 colors with 64GB
@@ -123,7 +121,7 @@ const Products = () => {
                   <div className="flex mt-4">
                     <div>
                       <p
-                        tabindex="0"
+                        tabIndex="0"
                         className="focus:outline-none text-xs text-gray-600 px-2 bg-gray-200 py-1"
                       >
                         12 months warranty
@@ -131,7 +129,7 @@ const Products = () => {
                     </div>
                     <div className="pl-2">
                       <p
-                        tabindex="0"
+                        tabIndex="0"
                         className="focus:outline-none text-xs text-gray-600 px-2 bg-gray-200 py-1"
                       >
                         Complete box
@@ -140,13 +138,13 @@ const Products = () => {
                   </div>
                   <div className="flex items-center justify-between py-4">
                     <h2
-                      tabindex="0"
+                      tabIndex="0"
                       className="focus:outline-none text-indigo-700 text-xs font-semibold"
                     >
                       Bay Area, San Francisco
                     </h2>
                     <h3
-                      tabindex="0"
+                      tabIndex="0"
                       className="focus:outline-none text-indigo-700 text-xl font-semibold"
                     ></h3>
                   </div>
